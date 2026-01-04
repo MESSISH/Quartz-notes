ebd5b962162991dedbecc7ebf27971c0a5d1dccb
MESSISH <1078580223@qq.com>
2026-01-04 23:54:14 +0800

    Change site title to MESSISH

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Quartz 4",
+    pageTitle: "MESSISH",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
